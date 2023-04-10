9b1b58e61a24a79daa551ae8e7543cfa0e1c2a6c
ncprieto <102989644+ncprieto@users.noreply.github.com>
2023-04-10 16:58:44 -0700

    folder structure/probuilder/simple movement/fmod

---
+++ Assets/Scripts/Player Related/MoveCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float sens;
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens * 100;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens * 100;
+        
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/Player Related/Movement.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform orientation;
+    public LayerMask ground;
+
+    [Header("Input")]
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+    [Header("Ground Movement Variables")]
+    public float acceleration;
+    public float deceleration;            
+    public float groundDrag;
+    public float jumpHeight;
+
+    void Start()
+    {
+        // sets deceleration to 1 if its less than 1, deceleration value less than 1 causes bugs
+        deceleration = deceleration < 1 ? 1 : deceleration;
+    }
+
+    bool isGrounded;
+    void Update()
+    {
+        // check if player is on the ground
+        RaycastHit hit;
+        isGrounded = Physics.SphereCast(orientation.position, 0.001f, -orientation.up, out hit, 1f, ground);
+        if(Input.GetKeyDown(jump)){ OnJumpPressed(); }
+    }
+
+    void FixedUpdate()
+    {
+        if(isGrounded)
+        {
+            rb.drag = groundDrag;
+            if(GetInputs())
+            {
+                Vector3 dir = GetWishDirection();
+                rb.velocity += dir * acceleration;
+            }
+            else
+            {
+                // slow player down if they aren't inputting anything if
+                // their currently velocty is > 0 ie player was just moving
+                if(rb.velocity.magnitude > 0)
+                {
+                    rb.velocity -=  rb.velocity / deceleration;
+                }
+            }
+        }
+        else if(!isGrounded)
+        {
+            rb.drag = 0;
+        }
+    }
+    
+    int fb;
+    int lr;
+    bool GetInputs()
+    {
+        // converts inputs into 0, 1, or -1 used for multiplying direction vectors
+        fb =  Input.GetKey(forward)  ? 1 : 0;
+        fb += Input.GetKey(backward) ? -1 : 0;
+
+        lr =  Input.GetKey(right) ? 1 : 0;
+        lr += Input.GetKey(left)  ? -1 : 0;
+
+        // return a boolean to see if the player is even inputing anything
+        return fb != 0 || lr != 0 ? true : false;
+    }
+
+    /* GetWishDirection() returns a Vector3 that points in the direction the
+     * player is wanting to move in based on their inputs and their transform.
+     * It returns a normalized direction vector.
+     */
+    Vector3 GetWishDirection()
+    {
+        Vector3 dir = orientation.forward * fb + orientation.right * lr;
+        return dir.normalized;
+    }
+
+    void OnJumpPressed()
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+}
